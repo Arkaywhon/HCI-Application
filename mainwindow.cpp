d238905d384599d67d74e1c406447287c9957f1f
OmarSyed99 <48013280+OmarSyed99@users.noreply.github.com>
2020-04-05 11:49:29 -0400

    Generates random binary

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,3 +13,22 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_pushButton_clicked()
+{
+    int high = pow(2,21);
+    int low = -1;
+    int randVal = qrand() % ((high + 1) - low) + low;
+    std::string bin;
+    //ui->numLabel->setText(QString::number(randVal));
+    while( randVal > 0 ) {
+            if( randVal % 2 == 0 ){
+                bin.insert( bin.begin( ), '0' );
+            }
+            else {
+                bin.insert( bin.begin( ), '1' );
+            }
+            randVal >>= 1;
+        }
+    ui->label->setText(QString::fromStdString(bin));
+}
